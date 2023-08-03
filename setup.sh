1a4d6dab1653e305ac6ee5fbe51a5d159771c8bb
Thuanvuse <88393748+Thuanvuse@users.noreply.github.com>
2023-08-03 22:57:09 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -61,7 +61,7 @@ EOF
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "Thuan$port/$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "huydz0$port/HuyBucu$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
